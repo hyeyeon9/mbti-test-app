9cdbdf0bb987eb7f6ae73d9bb1039753c24b9472
구혜연 <guhaa03@daum.net>
2025-06-13 01:57:43 +0900

    chore: 용하지 않는 loading 컴포넌트 제거

--- src/app/result/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="min-h-screen flex items-center justify-center seung-hoon-font">
-      <div className="animate-pulse">잠시만 기다료..</div>
-    </div>
-  );
-}
