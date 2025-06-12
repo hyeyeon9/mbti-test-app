ca2cb134757dfe371dd9f583eb15754b88efce04
구혜연 <guhaa03@daum.net>
2025-06-12 23:12:38 +0900

    feat: 테스트 소개 문구 추가 (

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,10 @@ export default function Home() {
       h-[500px]
        max-w-2xl  w-full text-center space-y-6"
       >
-        <h1 className="text-4xl font-bold text-gray-80 mb-20">MBTI 테스트</h1>
+        <h1 className="text-4xl font-bold text-gray-80 mb-5">MBTI 테스트</h1>
+        <p className="text-sm text-gray-500 mt-2">
+          팩폭과 귀여움의 경계선 테스트
+        </p>
         <NameInput />
       </div>
     </div>
