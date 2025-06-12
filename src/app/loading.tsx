2dedab168fcd5c3af7ea68cb10f7d8b6260873ec
구혜연 <guhaa03@daum.net>
2025-06-12 22:48:48 +0900

    feat : loading, not-found 페이지 구현

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="min-h-screen flex items-center justify-center seung-hoon-font">
+      <div className="animate-pulse">잠시만 기다료..</div>
+    </div>
+  );
+}
